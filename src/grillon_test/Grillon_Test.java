76500359b8149013519853095c72798707306b4e
JavaRatXIII <tamannaqureshi9@gmail.com>
2017-06-04 14:17:28 +0100

    Improvements
    Made the code a bit neater and nicer

--- src/grillon_test/Grillon_Test.java
+++ src/grillon_test/Grillon_Test.java
@@ -33,156 +33,51 @@ public class Grillon_Test {
         Likability_Rate = (getInput("Rate how likeable you are from 1 to 10 (10 being very likealbe):"));
         Happy_Rate = (getInput("Rate how happy they make you feel from 1 to 10 (10 being very happy):"));
         Like = JOptionPane.showInputDialog("Do you like them? [Yes] [No] [Maybe}");
+            if(Like.toLowerCase().equals("yes"))
+            {
+                points++;
+            }
         
         Age2 = (getInput("Enter their age:"));
+            if(Age > Age2+10)
+            {
+                System.out.println("Your too old");
+            }
+            else if(Age+12 < Age2)
+            {
+                System.out.println("They're too old");
+            }
+        
         GentleMen_Rate2 = (getInput("Rate how classy they are from 1 to 10 (10 being very classy):"));
+            points = Test(GentleMen_Rate,GentleMen_Rate2,points);
+            
         Kindness_Rate2 = (getInput("Rate how kind they are from 1 to 10 (10 being very kind):"));
+            points = Test(Kindness_Rate,Kindness_Rate2,points);
+            
         Trust_Rate2 = (getInput("Rate how turstworthy they are from 1 to 10 (10 being very turstworthy):"));
+            points = Test(Trust_Rate,Trust_Rate2,points);
+            
         Smart_Rate2 = (getInput("Rate how inteligent they are from 1 to 10 (10 being very inteligent):"));
+            points = Test(Smart_Rate,Smart_Rate2,points);
+            
         Strength_Rate2 = (getInput("Rate how strong they are from 1 to 10 (10 being very strong):"));
+            points = Test(Strength_Rate,Strength_Rate2,points);
+            
         Likability_Rate2 = (getInput("Rate how likeable they are from 1 to 10 (10 being very likealbe):"));
+            points = Test(Likability_Rate,Likability_Rate2,points);
+            
         Happy_Rate2 = (getInput("Rate how happy you make them feel from 1 to 10 (10 being very happy):"));
-        
-        
-        if(Age > Age2+10)
-        {
-            System.out.println("Your too old");
-        }
-        else if(Age+12 < Age2)
-        {
-            System.out.println("They're too old");
-        }
-        
-        
-        if(GentleMen_Rate > GentleMen_Rate2+3)
-        {
-            System.out.println("Your classier then them");
-        }
-        else if(GentleMen_Rate+3 < GentleMen_Rate2)
-        {
-            System.out.println("They're classier than you");
-        }
-        else
-        {
-            System.out.println("You're about as classy as they are");
-            points++;
-        }
-        
-        
-        if(Kindness_Rate > Kindness_Rate2+3)
-        {
-            System.out.println("You're very kind but don't let them hurt you");
-        }
-        else if(Kindness_Rate+3 < Kindness_Rate2)
-        {
-            System.out.println("They are nicer than you, so you must cherish them");
-            points++;
-        }
-        else
-        {
-            System.out.println("You're about as kind as they are");
-            points++;
-        }
-        
-        
-        if(Trust_Rate > Trust_Rate2+3)
-        {
-            System.out.println("Make sure they don't misuse your trust");
-            points++;
-        }
-        else if(Trust_Rate+3 < Trust_Rate2)
-        {
-            System.out.println("It's good that they're trustworthy");
-        }
-        else
-        {
-            System.out.println("You're about as trustworthy as they are");
-            points++;
-        }
-        
-        
-        if(Smart_Rate > Smart_Rate2+3)
-        {
-            System.out.println("They should study a little more");
-        }
-        else if(Smart_Rate+3 < Smart_Rate2)
-        {
-            System.out.println("You're about as smart as they are");
-        }
-        else
-        {
-            System.out.println("You're about as smart as they are");
-            points++;
-        }
-        
-        
-        if(Strength_Rate > Strength_Rate2+3)
-        {
-            System.out.println("Your stonger then them");
-        }
-        else if(Strength_Rate+3 < Strength_Rate2)
-        {
-            System.out.println("They're very strong");
-            points++;
-        }
-        else
-        {
-            System.out.println("You're about as strong as they are");
-            points++;
-        }
-        
-        
-        if(Likability_Rate > Likability_Rate2+3)
-        {
-            System.out.println("You're very likeable");
-            points++;
-        }
-        else if(Likability_Rate+3 < Likability_Rate2)
-        {
-            System.out.println("They're very likeable");
-            points++;
-        }
-        else
-        {
-            System.out.println("You're about as lieable as they are");
-            points++;
-        }
-        if((Likability_Rate < 3) || (Likability_Rate2 < 3))
-        {
-            points--;
-        }
-        
-        
-        if(Happy_Rate > Happy_Rate2+3)
-        {
-            System.out.println("They should try to make you happy as you make them");
-        }
-        else if(Happy_Rate+3 < Happy_Rate2)
-        {
-            System.out.println("It's good they make you happy, you should try making them happy too");
-            points++;
-        }
-        else
-        {
-            System.out.println("It's good you two are happy together");
-            points++;
-        }
-        
-        if((Like.equals("Yes")) || (Like.equals("yes")))
-        {
-            System.out.println("You like them so maybe you should keep trying");
-            points++;
-        }
-        else if((Like.equals("No")) || (Like.equals("no")))
-        {
-            System.out.println("You don't even like them");
-        }
-        else
-        {
-            System.out.println("You should try to understand what you feel for them");
-        }
+            points = Test(Happy_Rate,Happy_Rate2,points);
         
         GrillonTest(points);
+    }
+    public static int Test(int quiztakerval, int personval, int points)
+    {
+        if((quiztakerval == personval) || (quiztakerval+1 == personval) || (quiztakerval-1 == personval) || (quiztakerval == personval-1) || (quiztakerval == personval+1))
+        {
+            points++;
+        }
+        return points;
     }
     
     public static int getInput(String word)
@@ -193,17 +88,17 @@ public class Grillon_Test {
     
     public static void GrillonTest(int points)
     {
-        if(points >= 3)
+        if(points <= 3)
         {
-            JOptionPane.showMessageDialog(null, "The tow of you scaored a " + points + "You two don't seam very compatible");
+            JOptionPane.showMessageDialog(null, "The two of you scaored a " + points + " You two don't seam very compatible. I'm not saying you should give up, but if you think they're worth it you need to try harder.");
         }
         else if((points <= 4) && (points <= 6))
         {
-            JOptionPane.showMessageDialog(null, "The tow of you scaored a " + points + "You two may work but you both should try harder to avoid failur");
+            JOptionPane.showMessageDialog(null, "The two of you scaored a " + points + " You two may work but you both should try harder to avoid problems in the future");
         }
         else if(points >= 7)
         {
-            JOptionPane.showMessageDialog(null, "The two of you scaored a " + points + "You two are so good together");
+            JOptionPane.showMessageDialog(null, "The two of you scaored a " + points + " You two are so good together");
         }
     }
     
